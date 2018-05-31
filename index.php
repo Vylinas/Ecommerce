d0c9b08c23627eeab1a3306a3945d54c05fd0ce1
Unknown <sylvain4iphone@gmail.com>
2018-05-31 11:27:04 +0200

    Install SCSS
    just do "sass setup.scss:style.css" in "css/" folder with CLI

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Link for Vue.js -->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <!-- Link for Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <!-- Link for CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
-    
+<script>
+
+new Vue({
+    el: '#block',
+    data: {
+        pseudo: user.pseudo,
+    }
+})
+
+</script>
 </body>
 </html>
