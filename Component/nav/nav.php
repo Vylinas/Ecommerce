21ff64e22fcdc78b555bb7f00f19ba3c8ef217cd
Vylinas <sylvain4iphone@gmail.com>
2018-06-13 17:29:00 +0200

    Nav
    Works : Not connect to DB.

--- Component/nav/nav.php
+++ Component/nav/nav.php
@@ -5,9 +5,14 @@ require '../../vendor/autoload.php';
 require '../../Entity/ItemNav.class.php';
 
 $loader = new Twig_Loader_Filesystem('tpl/');
-$twig = new Twig_Environment($loader);
+$twig = new Twig_Environment($loader, array(
+  'debug' => true,
+));
+$twig->addExtension(new Twig_Extension_Debug()); // va de paire avec la config "debug" dans l'Environement
 
-$array_items_menu = array();
+
+$first_lvl_menu = array();
+$second_lvl_menu = array();
 
 $item1 = new ItemNav;
 $item1->setId(0);
@@ -16,7 +21,7 @@ $item1->setLink("/");
 $item1->setSubMenu(true);
 $item1->setParentMenu(false);
 $item1->setActive("active");
-$array_items_menu[] = $item1;
+$first_lvl_menu[] = $item1;
 
 $item2 = new ItemNav;
 $item2->setId(1);
@@ -25,7 +30,7 @@ $item2->setLink("/propos");
 $item2->setSubMenu(false);
 $item2->setParentMenu("0");
 $item2->setActive("active");
-$array_items_menu[] = $item2;
+$first_lvl_menu[] = $item2;
 
 $item3 = new ItemNav;
 $item3->setId(2);
@@ -34,7 +39,7 @@ $item3->setLink("/produit");
 $item3->setSubMenu(true);
 $item3->setParentMenu(false);
 $item3->setActive("active");
-$array_items_menu[] = $item3;
+$first_lvl_menu[] = $item3;
 
 $item4 = new ItemNav;
 $item4->setId(3);
@@ -43,9 +48,53 @@ $item4->setLink("/t-shirt");
 $item4->setSubMenu(false);
 $item4->setParentMenu("2");
 $item4->setActive("active");
-$array_items_menu[] = $item4;
+$first_lvl_menu[] = $item4;
 
+$item5 = new ItemNav;
+$item5->setId(4);
+$item5->setTitle("Jean");
+$item5->setLink("/jean");
+$item5->setSubMenu(false);
+$item5->setParentMenu("2");
+$item5->setActive("active");
+$first_lvl_menu[] = $item5;
+
+$item6 = new ItemNav;
+$item6->setId(5);
+$item6->setTitle("Autre");
+$item6->setLink("/autre");
+$item6->setSubMenu(false);
+$item6->setParentMenu(false);
+$item6->setActive("active");
+$first_lvl_menu[] = $item6;
+
+$item7 = new ItemNav;
+$item7->setId(6);
+$item7->setTitle("Blop");
+$item7->setLink("/blop");
+$item7->setSubMenu(false);
+$item7->setParentMenu("2");
+$item7->setActive("active");
+$first_lvl_menu[] = $item7;
+
+$item8 = new ItemNav;
+$item8->setId(7);
+$item8->setTitle("T-shirt");
+$item8->setLink("/t-shirt");
+$item8->setSubMenu(false);
+$item8->setParentMenu("2");
+$item8->setActive("active");
+$first_lvl_menu[] = $item8;
+
+$item9 = new ItemNav;
+$item9->setId(8);
+$item9->setTitle("T-shirt");
+$item9->setLink("/t-shirt");
+$item9->setSubMenu(false);
+$item9->setParentMenu("2");
+$item9->setActive("active");
+$first_lvl_menu[] = $item9;
 
 echo $twig->render('test.html.twig', array(
-  'items' => $array_items_menu,
+  'items' => $first_lvl_menu,
   ));
